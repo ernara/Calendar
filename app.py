b2a1f1d1af8b9cf21d6f5c793f49d07bf84f0e9d
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2024-06-02 02:29:44 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/events')
+def events():
+    with open('events.json', 'r', encoding='utf-8') as f:
+        events_data = json.load(f)
+    
+    num_events = len(events_data)  # Get the number of events
+    
+    response = {
+        'num_events': num_events,
+        'events_data': events_data
+    }
+    
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
